87bb8f312ab8d1d00358f3149a25f97e0a49024b
DanielRendon1020 <d.danny1020@gmail.com>
2023-05-17 21:36:33 -0700

    button style changed using context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import './App.css';
-
-import UserInterface from "./UserInterface"
+import React from "react";
+import "./App.css";
+import StyleContext from "./StyleContext";
+import UserInterface from "./UserInterface";
 
 function App() {
+  const styling = {
+    background: "#1ECD97",
+    display: "inline-block",
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #1ECD97",
+    borderRadius: 15,
+    margin: ".5rem",
+    boxShadow: "0px 0px 8px #1ECD97",
+  };
+
   return (
-    <main>
-     <h1
+    <main className="App">
+      <StyleContext.Provider value={styling}>
+        <h1
           style={{
-            color: "#FEE001",
+            textShadow: "2px 2px 10px #1ECD97",
             fontFamily: "Luminari, fantasy",
-            fontSize: 50
+            fontSize: 60,
           }}
         >
           Welcome to Comics Galore!
         </h1>
         <UserInterface />
+      </StyleContext.Provider>
     </main>
   );
 }
--- src/RequestFormButton.jsx
+++ src/RequestFormButton.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
-
+import StyleContext from "./StyleContext";
 
 export default function RequestFormButton() {
+  const styling = useContext(StyleContext);
+
   return (
     <div>
-      <button>
+      <button style={styling}>
         Click Here!
       </button>
-      <input type="submit" value="Submit" />
+      <input style={styling} type="submit" value="Submit" />
 
     </div>
   );
